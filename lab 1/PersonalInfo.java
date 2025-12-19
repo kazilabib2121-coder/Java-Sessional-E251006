ac99b27bdcd557077680e63b47989ae96975f048
kazilabib2121-coder <kazilabib2121@gmail.com>
2025-12-19 20:39:16 +0600

    Update PersonalInfo.java

--- lab 1/PersonalInfo.java
+++ lab 1/PersonalInfo.java
@@ -2,12 +2,11 @@ public class PersonalInfo {
     public static void main(String[] args) {
         String name = "Kazi Labib";
         String studentId = "E251006";
-        int age = 20;
-        String email = "kazilabib2121@gmail.com";
-        String phone = "017XXXXXXXX";
-        String address = "Mirsarai,Chattogram";
+        int age = 21;
+        String email = "labib807067@gmail.com";
+        String phone = "016XXXXXXXX";
+        String address = "Mirsarai, Chattogram";
 
-        
         System.out.println("Personal Information:");
         System.out.println("Name: " + name);
         System.out.println("Student ID: " + studentId);
@@ -15,14 +14,5 @@ public class PersonalInfo {
         System.out.println("Email: " + email);
         System.out.println("Phone: " + phone);
         System.out.println("Address: " + address);
-
-        System.out.printf("\nFormatted Personal Information:\n");       
-        System.out.printf("Name: %s\n", name);
-        System.out.printf("Student ID: %s\n", studentId);   
-        System.out.printf("Age: %d\n", age);
-        System.out.printf("Email: %s\n", email);        
-        System.out.printf("Phone: %s\n", phone);
-        System.out.printf("Address: %s\n", address);
-                
     }
 }
